2c2dbc36bbc36e685db7ed96ee9b4bf7b954a574
chetanpatidar31 <chetan50682@gmail.com>
2025-04-07 17:29:03 +0530

    intial

---
+++ jdbc-advance/src/com/rays/jdbc/TestConnection.java
@@ -0,0 +1,33 @@
+package com.rays.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestConnection {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// step 1 load driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		// step 2 connection with database
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
+
+		// step 3 create statement
+		PreparedStatement pstmt = conn.prepareStatement("select * from employee");
+
+//		step 4 execute query and get resultset
+		ResultSet rs = pstmt.executeQuery();
+
+		while (rs.next()) {
+			System.out.print("\t" + rs.getInt(1));
+			System.out.print("\t \t" + rs.getString(2));
+			System.out.print("\t \t" + rs.getInt(3));
+			System.out.print("\t \t" + rs.getString(4));
+			System.out.println("\t \t" + rs.getInt(5));
+		}
+
+	}
+}
